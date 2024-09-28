0346224d75f3313af6968be46c1af436e17e24fb
Kings Uthana Idogu <kingsuthanaidogu@gmail.com>
2024-09-28 13:21:25 +0100

    Add project files.

---
+++ SuperHeroAPI-DotNet8/Controllers/SuperHeroController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet8.Data;
+
+namespace SuperHeroAPI_DotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public SuperHeroController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = await _dataContext.SuperHeroes.ToListAsync();
+            
+
+             return Ok(heroes);
+
+            
+        }
+        [HttpGet("{id}")]
+        
+        public async Task<ActionResult<List<SuperHero>>> GetHero(int id)
+        {
+            var hero = await _dataContext.SuperHeroes.FindAsync(id);
+            if (hero is null)
+                return NotFound("Hero not found.");
+           
+            return Ok(hero);
+
+
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            _dataContext.SuperHeroes.Add(hero);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await GetAllHeroes());
+
+
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero updatedHero)
+        {
+            var dbHero = await _dataContext.SuperHeroes.FindAsync(updatedHero.Id);
+            if (dbHero is null)
+                return NotFound("Hero not found.");
+
+            dbHero.Name = updatedHero.Name;
+            dbHero.FirstName = updatedHero.FirstName;
+            dbHero.LastName = updatedHero.LastName;
+            dbHero.Place = updatedHero.Place;
+
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await GetAllHeroes());
+
+
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var dbHero = await _dataContext.SuperHeroes.FindAsync(id);
+            if (dbHero is null)
+                return NotFound("Hero not found.");
+
+           _dataContext.SuperHeroes.Remove(dbHero);
+
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+
+
+        }
+
+
+    }
+}
---
+++ SuperHeroAPI-DotNet8/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI_DotNet8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        // Define your DbSets here
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
